f61778a60d432c36ae96f388a3371697cadf67cb
Louisloode <louisdebraine@gmail.com>
2016-04-29 18:42:33 +0200

    Add comments, debug post

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/*
+* CONFIGURATION
+*/
+
+// Init packages
 var express = require('express')
 var session = require('express-session')
 var bodyParser = require('body-parser')
@@ -7,25 +12,46 @@ var http = require("https")
 var Grant = require('grant-express'), 
 grant = new Grant(require('./config/config.json'))
 var jsonfile = require('jsonfile')
-var file = './tmp/data.json'
+var file = './tmp/data.json' // Cache file for
 
+// Ini app
 var app = express()
 
-// REQUIRED:
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+/*
+* MIDDLEWARES
+*/
+
+// 
 app.use(session({secret:'very secret'}))
 // mount grant
 app.use(grant)
 
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
+/*
+* ROUTES
+*/
+
+// Index
 app.get('/', function (req, res) {
+  // Render ejs
   res.render('pages/index');
 });
 
+// Callback with list of channels
 app.get('/handle_callback', function (req, res) {
+
+  // Init cache file for channel listings
+  /*var content = '';
+  jsonfile.writeFile(file, content, function (err) {
+            console.error(err)
+  })*/
+
+  // Check if the request work
   if(req.query.raw.ok == 'false'){
 
     // Send error message
@@ -44,32 +70,37 @@ app.get('/handle_callback', function (req, res) {
       'headers': {}
     };
 
+    // http request
+    var req = http.request(options, function (res) {
+    var chunks = [];
 
-    http.get(options, function(res) {
-      console.log("Got response: " + res.statusCode);
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
 
-        res.on("data", function(res) {
-          console.log("BODY: " + res);
-          var data = JSON.parse(res);
-          var channels = data.channels;
-          console.log(channels);
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        var data = JSON.parse(body);
+        var channels = data.channels;
+
+          //console.log(channels);
+          // write result in cache file
           jsonfile.writeFile(file, channels, function (err) {
             console.error(err)
           })
-        });
 
-        res.on("end", function(res) {
-          //console.log("BODY: " + res);
-        });
-
-
-    }).on('error', function(e) {
-      console.log("Got error: " + e.message);
+        console.log(body.toString());
+      });
     });
 
+    req.end();
+  
+
+    // read cache file with channels list
     var channels = jsonfile.readFileSync(file);
-      console.log(channels);
-    // Render
+      //console.log(channels);
+  
+    // Render ejs
     res.render('pages/dashboard', {
               team_name: team_name,
               channels: channels,
@@ -79,20 +110,21 @@ app.get('/handle_callback', function (req, res) {
 })
 
 
-
+// GET page post
 app.get('/channels/:channelId/post', function (req, res) {
   var token = req.query.token;
   var channelId = req.params.channelId;
   //console.log(token);
   //console.log(channelId);
   var msg = '';
-    res.render('pages/post', {token: token, msg:msg});
+  res.render('pages/post', {token: token, msg:msg});
 });
 
+// POST message on specific channel
 app.post('/channels/:channelId/post', function (req, res) {
   var token = req.query.token;
   var channelId = req.params.channelId;
-  var message = req.body.message;
+  var message = encodeURIComponent(req.body.message);
   //console.log(message);
 
   var options = {
@@ -103,6 +135,7 @@ app.post('/channels/:channelId/post', function (req, res) {
     'headers': {}
   };
 
+  // http request
   var req = http.request(options, function (res) {
     var chunks = [];
 
@@ -117,11 +150,13 @@ app.post('/channels/:channelId/post', function (req, res) {
   });
 
   req.end();
+  // Message
   var msg = 'The message has been send';
+  // Render ejs
   res.render('pages/post', {msg:msg});
 });
 
-
+// Listening App on port 3000
 app.listen(3000, function () {
   console.log('Express server listening on port ' + 3000)
 })
